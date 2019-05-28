0ff96da2dbde642497074873b1917eba475ed12a
C. Bates <cbates1@live.com>
2019-05-28 19:12:36 -0400

    Unit tests pass.

--- StolenBreakfastDrone/Program.cs
+++ StolenBreakfastDrone/Program.cs
@@ -17,10 +17,39 @@ namespace StolenBreakfastDrone
     {
         public static int FindUniqueDeliveryId(int[] deliveryIds)
         {
+            Dictionary<int, int> suspects = new Dictionary<int, int>();
             // Find the one unique ID in the array
+            // Iterate the deliveryIds, creating a dictionary.  Find the id(s) in dictionary with < 2 mentions.
+            foreach (int id in deliveryIds)
+            {
+                if (suspects.Keys.Contains(id))
+                {
+                    suspects[id]++;
+                }
+                else
+                {
+                    suspects.Add(id, 1);
+                }
+            }
 
+            var one = suspects.Where((LessThanTwoMentions)).ToList() ;
+
+            if (one.Any( ))
+            {
+                return one.First().Key;
+            }
 
             return 0;
+        }
+
+        private static bool LessThanTwoMentions(KeyValuePair<int, int> arg)
+        {
+            if (arg.Value < 2)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
